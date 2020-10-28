1961034709109604bfb6de21e7e14d732a3cb68c
2suke <takozawa3@icloud.com>
2020-10-29 02:37:06 +0900

    add db backup

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>Rotatebox</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php
+    $link = mysqli_connect('localhost', 'takezawa', 'sekkapy0401', 'rotate_box');
+
+    // 接続状況をチェックします
+    if (mysqli_connect_errno()) {
+        die("データベースに接続できません:" . mysqli_connect_error() . "\n");
+    } else {
+        echo "データベースの接続に成功しました。\n";
+    }
+ ?> 
  </body>
 </html>
