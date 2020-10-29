a22f61263b1c78e9655622a15c9ea41237eb3acc
2suke <takozawa3@icloud.com>
2020-10-29 09:14:12 +0900

    add PDO data accesser

---
+++ db/data_access/data_access.php
@@ -0,0 +1,27 @@
+<?php
+
+class DataAccesser {
+  public $dbh;
+
+  function dbConnect(){
+    $dsn = 'mysql:dbname=rotate_box;host=localhost';
+    $user = 'takezawa';
+    $password = 'sekkapy0401';
+
+    try {
+      $dbh = new PDO($dsn, $user, $password);
+      echo "接続成功\n";
+    } catch (PDOException $e) {
+      echo "接続失敗: " . $e->getMessage() . "\n";
+      exit();
+    }
+  }
+
+  function dbClose(){
+    $dbh = null;
+    if(is_null($dbh)){
+      echo "切断しました";
+    }
+  }
+}
+?>
--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <html>
- <head>
-  <title>Rotatebox</title>
- </head>
- <body>
- <?php
-    $link = mysqli_connect('localhost', 'takezawa', 'sekkapy0401', 'rotate_box');
-
-    // 接続状況をチェックします
-    if (mysqli_connect_errno()) {
-        die("データベースに接続できません:" . mysqli_connect_error() . "\n");
-    } else {
-        echo "データベースの接続に成功しました。\n";
-    }
- ?> 
- </body>
+	<head>
+		<title>Rotatebox</title>
+	</head>
+	<body>
+	<?php
+		require("db/data_access/data_access.php");
+		
+		$dao = new DataAccesser();
+		$dao->dbConnect();
+		$dao->dbClose();
+	?> 
+	</body>
 </html>
